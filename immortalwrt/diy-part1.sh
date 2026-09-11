52f45dbb9b66fc836d5323076c23ff6a0e5014f4
fanling1970 <zghqidai2008@sina.com>
2026-09-11 16:28:40 +0800

    Clone luci-app-aurora-config and luci-theme-aurora

--- immortalwrt/diy-part1.sh
+++ immortalwrt/diy-part1.sh
@@ -1,1 +1,3 @@
 #!/usr/bin/env bash
+git clone --depth 1 https://github.com/eamonxg/luci-app-aurora-config.git package/luci-app-aurora-config
+git clone --depth 1 https://github.com/eamonxg/luci-theme-aurora.git package/luci-theme-aurora
